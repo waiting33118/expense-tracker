1f7b1f349db34bd7d191a6d005541afc5193c747
Tony <waiting33118@gmail.com>
2020-05-19 16:47:58 +0800

    feat: add homePage UI

--- routes/modules/homePage.js
+++ routes/modules/homePage.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const router = express.Router()
+const Category = require('../../models/Category')
+const Record = require('../../models/Record')
 
 router.get('/', (req, res) => {
 	res.render('home')
