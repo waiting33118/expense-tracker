0d6ef5056f7a4ffb5c32ed43a746d8662bcca2cd
Tony <waiting33118@gmail.com>
2020-05-19 16:46:18 +0800

    feat: use static file

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const PORT = process.env.PORT || 3000
 app.engine('handlebars', exphdbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 app.use(routes)
+app.use(express.static('public'))
 
 app.listen(PORT, () =>
 	console.log(`The Server is running on http://localhost:${PORT}`)
