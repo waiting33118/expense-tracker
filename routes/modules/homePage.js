796e321fc4e78a95ad40c68028d8aac0547b7748
Tony <waiting33118@gmail.com>
2020-05-19 22:50:10 +0800

    feat: add homePage GET function

--- routes/modules/homePage.js
+++ routes/modules/homePage.js
@@ -1,10 +1,20 @@
 const express = require('express')
 const router = express.Router()
-const Category = require('../../models/Category')
 const Record = require('../../models/Record')
+const category = ['家居物業', '交通出行', '休閒娛樂', '餐飲食品', '其他']
 
 router.get('/', (req, res) => {
-	res.render('home')
+	Record.find()
+		.sort('_id')
+		.lean()
+		.then((record) => {
+			let totalAmount = 0
+			record.forEach((item) => {
+				totalAmount += item.amount
+			})
+			res.render('home', { record, category, totalAmount })
+		})
+		.catch((err) => console.log(err))
 })
 
 module.exports = router
