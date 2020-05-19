dfcefcb6d9fd3be836a55fc2c121868b55443aab
Tony <waiting33118@gmail.com>
2020-05-19 22:45:21 +0800

    feat: require expense route

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const homePage = require('./modules/homePage')
-
+const expensePage = require('./modules/expense')
 //首頁
 router.use('/', homePage)
+router.use('/expense', expensePage)
 
 module.exports = router
