82494fdb7e6b187960be58796ef52e036aa8aa57
Tony <waiting33118@gmail.com>
2020-05-20 16:52:03 +0800

    feat: import and set params => bodyParser & method Override

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const exphdbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
 const app = express()
 require('./configs/mongoose')
 const routes = require('./routes')
@@ -7,6 +9,8 @@ const PORT = process.env.PORT || 3000
 
 app.engine('handlebars', exphdbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 app.use(routes)
 app.use(express.static('public'))
 
