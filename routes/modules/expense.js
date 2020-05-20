da7af2fb4385e1c5928661b1cfb51fd45abc2e77
Tony <waiting33118@gmail.com>
2020-05-20 23:09:29 +0800

    feat: create POST new & GET edit function

--- routes/modules/expense.js
+++ routes/modules/expense.js
@@ -3,14 +3,30 @@ const router = express.Router()
 const Record = require('../../models/Record')
 const category = ['家居物業', '交通出行', '休閒娛樂', '餐飲食品', '其他']
 
-//新增記帳
+//渲染頁面-新增記帳
 router.get('/new', (req, res) => {
 	res.render('createNewExpense', { category })
 })
 
-//接收新增記帳表單
+//處理表單-新增記帳
 router.post('/new', (req, res) => {
-	console.log(req.body)
+	const { name, date, category, amount } = req.body
+	Record.create({
+		name,
+		date,
+		category,
+		amount,
+	})
+		.then(() => res.redirect('/'))
+		.catch((err) => console.log(err))
+})
+
+//渲染頁面-修改記帳
+router.get('/:_id/edit', (req, res) => {
+	const id = req.params._id
+	Record.findById(id)
+		.lean()
+		.then((record) => res.render('editExpense', { record, category }))
 })
 
 module.exports = router
