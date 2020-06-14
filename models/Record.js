bc79c36be56e3873f295780dfb8c1093a5cc5592
Tony <waiting33118@gmail.com>
2020-06-14 11:59:58 +0800

    feat: add new Schema items: merchant,userId

--- models/Record.js
+++ models/Record.js
@@ -2,22 +2,32 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const recordSchema = new Schema({
-	name: {
-		type: String,
-		required: true,
-	},
-	category: {
-		type: String,
-		required: true,
-	},
-	date: {
-		type: String,
-		required: true,
-	},
-	amount: {
-		type: Number,
-		required: true,
-	},
+  name: {
+    type: String,
+    required: true
+  },
+  merchant: {
+    type: String,
+    required: true
+  },
+  category: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: String,
+    required: true
+  },
+  amount: {
+    type: Number,
+    required: true
+  },
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    index: true,
+    required: true
+  }
 })
 
 module.exports = mongoose.model('Record', recordSchema)
