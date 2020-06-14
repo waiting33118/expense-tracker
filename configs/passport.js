8bfa7439aa932c1aa120e68f7adc4e07cc814aa6
Tony <waiting33118@gmail.com>
2020-06-14 11:58:53 +0800

    feat: create passport config

---
+++ configs/passport.js
@@ -0,0 +1,48 @@
+const passport = require('passport')
+const bcrypt = require('bcryptjs')
+const User = require('../models/User')
+const LocalStrategy = require('passport-local').Strategy
+
+module.exports = (app) => {
+  app.use(passport.initialize())
+  app.use(passport.session())
+
+  passport.use(
+    new LocalStrategy(
+      { usernameField: 'email', passReqToCallback: true },
+      (req, email, password, done) => {
+        User.findOne({ email })
+          .then((user) => {
+            if (!user) {
+              return done(
+                null,
+                false,
+                req.flash('alert_msg', '此帳號尚未註冊！')
+              )
+            }
+            bcrypt
+              .compare(password, user.password)
+              .then((isMatch) => {
+                if (!isMatch) {
+                  return done(null, false, req.flash('alert_msg', '密碼錯誤'))
+                }
+              })
+              .catch((err) => done(err, false))
+            return done(null, user)
+          })
+          .catch((err) => done(err, false))
+      }
+    )
+  )
+
+  passport.serializeUser((user, done) => {
+    done(null, user.id)
+  })
+
+  passport.deserializeUser((_id, done) => {
+    User.findById({ _id })
+      .lean()
+      .then((user) => done(null, user))
+      .catch((err) => done(err, false))
+  })
+}
