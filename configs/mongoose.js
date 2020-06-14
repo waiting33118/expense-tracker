36e8264978088eebea40ee819e58f4f1ed30aabc
Tony <waiting33118@gmail.com>
2020-06-14 11:58:32 +0800

    feat: use dotenv params

--- configs/mongoose.js
+++ configs/mongoose.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose')
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/Expense'
 
-mongoose.connect(MONGODB_URI, {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true
 })
 
 const db = mongoose.connection
