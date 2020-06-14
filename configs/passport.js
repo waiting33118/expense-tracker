477e00e4926ea1bd6b209d34e7a454a35aacdbc7
Tony <waiting33118@gmail.com>
2020-06-14 14:37:26 +0800

    feat: edit password check function

--- configs/passport.js
+++ configs/passport.js
@@ -23,12 +23,12 @@ module.exports = (app) => {
             bcrypt
               .compare(password, user.password)
               .then((isMatch) => {
-                if (!isMatch) {
-                  return done(null, false, req.flash('alert_msg', '密碼錯誤'))
+                if (isMatch) {
+                  return done(null, user)
                 }
+                return done(null, false, req.flash('alert_msg', '密碼錯誤'))
               })
               .catch((err) => done(err, false))
-            return done(null, user)
           })
           .catch((err) => done(err, false))
       }
@@ -36,7 +36,7 @@ module.exports = (app) => {
   )
 
   passport.serializeUser((user, done) => {
-    done(null, user.id)
+    done(null, user._id)
   })
 
   passport.deserializeUser((_id, done) => {
